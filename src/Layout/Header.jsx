424786958ea59118058d96a9eed348e07d288778
Suraj Prasad-13 <spyadav0413@gmail.com>
2025-07-18 18:32:29 +0530

    updated the dashboard

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -5,92 +5,12 @@ import { IoMenu } from "react-icons/io5";
 import { GoPeople } from "react-icons/go";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaArrowTrendUp } from "react-icons/fa6";
-import { CiCalendar } from "react-icons/ci";
 import { MdOutlinePeopleAlt } from "react-icons/md";
+import { CiCalendar } from "react-icons/ci";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { LuCalendar } from "react-icons/lu";
 
 
-const cards = [
-    {
-      id: 1,
-      title: "Today's Revenue",
-      number: "$12,450",
-      icon: <FaArrowTrendUp />,
-      percentage: "+15.2%",
-    },
-    {
-      id: 2,
-      title: "Room Occupancy",
-      number: "87%",
-      icon: <LuCalendar  />,
-      percentage: "+5.2%",
-    },
-    {
-      id: 3,
-      title: "Active Orders",
-      number: 23,
-      icon:<FaArrowTrendUp />,
-      percentage: "-2.1%",
-    },
-    {
-      id: 4,
-      title: "Total Guests",
-      number: 156,
-      icon: <MdOutlinePeopleAlt />,
-      percentage: "+8.7%",
-    },
-  ]
-
-  //  third row ke firstpart
-   const firstcard = [
-    {
-      
-      discription:'New Booking confirmed for Room 205',
-      time:'2 mins ago',
-      status:'high',
-    },
-    {
-      
-      discription:'Order #ORD-1234 ready for service',
-      time:'5 mins ago',
-      status:'medium',
-    },
-    {
-      
-      discription:'Guest John Smith chekced out',
-      time:'10 mins ago',
-      status:'low',
-    },
-    {
-      
-      discription:'Rooms 105 maintenance completed',
-      time:'15 mins ago',
-      status:'medium',
-    },
-   ]
-
-  // third row ka second part
-
-  const secondcard = [
-    {
-      title:'Check-ins',
-      number:12,
-    },
-    {
-      title:'Check-outs',
-      number:8,
-    },
-    {
-      title:'Food Orders',
-      number:45,
-    },
-    {
-      title:'Revenue',
-      number:'$12,450',
-    },
-
-  ]
 
 
 
@@ -179,109 +99,7 @@ const Header = () => {
 
 
 
-<div className="bg-white shadow    p-5">
-       {/* first row */}
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 m-3 gap-2 ">
-          {
-            cards.map((card, index) => {
-              return (
-                <div key={index}
-                  className="border border-gray-300  shadow-md p-2 flex justify-between items-center rounded-md">
-                  <div className="flex flex-col" >
-                    <p className="text-gray-500">{card.title}</p>
-                    <p className="font-bold">{card.number}</p>
-                    <p
-                      className={`text-sm ${card.percentage.startsWith('+') ? 'text-green-600' : 'text-red-600'
-                        }`}
-                    >
-                      {card.percentage}
-                    </p>
-                  </div>
-                  <div className={`text-lg p-2 rounded-full ${index === 0 ? 'text-green-600  bg-green-100' : ''} ${index === 1 ? 'text-blue-600  bg-blue-100' : ''} ${index === 2 ? 'text-purple-600  bg-purple-100' : ''} ${index === 3 ? 'text-yellow-600  bg-yellow-100' : ''}`}>
-                    {card.icon}
-                  </div>
-                </div>
-              )
-            }
-            )
-          }
-        </div>
-        
-        {/* second row */}
-        <div className="flex flex-col justify-start m-3 rounded-md shadow-md p-3 gap-3">
-           <h1 className="font-bold text-lg col-span-1">Qucik Actions</h1>
-           <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3">
-                 <div className="rounded flex flex-col justify-center border border-gray-300 items-center transition-all duration-400 hover:text-white hover:bg-black p-3 ">
-                    <LuCalendar  />
-                    <p className="font-semibold">New Booking</p>
-                 </div>
-                 <div className="rounded flex flex-col justify-center items-center border border-gray-300 transition-all duration-400 hover:text-white hover:bg-black p-3">
-                    <GiHamburgerMenu />
-                    <p className="font-semibold">Add Order</p>
-                 </div>
-                 <div className="rounded flex flex-col justify-center items-center border border-gray-300 transition-all duration-400 hover:text-white hover:bg-black p-3">
-                   <MdOutlinePeopleAlt />
-                    <p className="font-semibold">Check in</p>
-                 </div>
-                 <div className="rounded flex flex-col justify-center items-center border border-gray-300 transition-all duration-400 hover:text-white hover:bg-black p-3">
-                    <MdOutlinePeopleAlt />
-                    <p className="font-semibold">Reports</p>
-                 </div>
-                 
-           </div>
-        </div>
 
-        {/* third row */}
-        
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 m-5 gap-4">
-              {/* first card */}
-               <div className="p-3 roudned shadow-md flex flex-col justify-start gap-2">
-                  <h1 className="font-bold text-lg">Recent Activites</h1>
-                   {
-                    firstcard.map((first,index)=>{
-                      return (
-                        <div key={index} className="bg-gray-50 rounded p-3 flex justify-between items-center">
-                              <div>
-                                <p className="font-semibold">{first.discription}</p>
-                                <span>{first.time}</span>
-                              </div>
-                              <div className={` py-1 px-3 font-semibold rounded-full ${first.status === 'high' ? 'bg-red-100 text-red-900 ' : '' } ${first.status === 'medium' ? 'bg-yellow-100 text-yellow-900 ' : '' } ${first.status === 'low' ? 'bg-green-100 text-green-900 ' : '' } ${first.status === 'high' ? 'bg-red-100 text-red-900 ' : '' }
-                              `}>
-                                {first.status}
-                              </div>
-                        </div>
-                      )
-                    })
-                   }
-               </div>
-
-               {/* second card */}
-
-               <div className="p-3 roudned shadow-md flex flex-col justify-start gap-2">
-                  <h1 className="font-bold text-lg">Today's Summary</h1>
-                   {
-                    secondcard.map((second,index)=>{
-                      return (
-                        <div key={index} className={` rounded p-3 flex justify-between items-center ${index === 0  ? 'bg-blue-50' : ''} ${index === 1  ? 'bg-green-50' : ''} ${index === 2  ? 'bg-purple-50' : ''} ${index === 3  ? 'bg-orange-50' : ''}
-                        `}>
-                              <div>
-                                <p className="font-semibold">{second.title}</p>
-                                
-                              </div>
-                              <div className={` py-1 px-3 font-semibold rounded-full ${second.number === 12 ? ' text-blue-700 ' : '' } ${second.number === 8 ? ' text-green-700 ' : '' } ${second.number === 45 ? ' text-purple-700 ' : '' } ${second.number === '$12,450' ? ' text-orange-700 ' : '' }
-                              `}>
-                                {second.number}
-                              </div>
-                        </div>
-                      )
-                    })
-                   }
-               </div>
-        </div>
-
-
-
-      </div>
 
 
 
